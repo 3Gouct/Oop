6923ca5c2af5555760e86544c0b32c68271c67ef
3Gouct <ilya_football@mail.ru>
2022-05-29 22:42:14 +0300

    adding tests

--- src/test/java/ru/netology/statistic/RadioTest.java
+++ src/test/java/ru/netology/statistic/RadioTest.java
@@ -20,6 +20,30 @@ public class RadioTest {
     }
 
     @Test
+    void setMinCurrentRadioStationNumber() {
+        Radio rad = new Radio ();
+
+        rad.setCurrentRadioStationNumber (10);
+
+        int actual = rad.getCurrentRadioStationNumber ();
+        int expected = 0;
+
+        assertEquals (expected, actual);
+    }
+
+    @Test
+    void setMaxCurrentRadioStationNumber() {
+        Radio rad = new Radio ();
+
+        rad.setCurrentRadioStationNumber (-1);
+
+        int actual = rad.getCurrentRadioStationNumber ();
+        int expected = 0;
+
+        assertEquals (expected, actual);
+    }
+
+    @Test
     void nextRadioStationNumber() {
         Radio rad = new Radio ();
         rad.setCurrentRadioStationNumber (1);
@@ -33,6 +57,19 @@ public class RadioTest {
     }
 
     @Test
+    void more() {
+        Radio rad = new Radio ();
+        rad.setCurrentRadioStationNumber (9);
+
+        rad.nextRadioStationNumber ();
+
+        int actual = rad.getCurrentRadioStationNumber ();
+        int expected = 0;
+
+        assertEquals (expected, actual);
+    }
+
+    @Test
     void prevRadioStationNumber() {
         Radio rad = new Radio ();
         rad.setCurrentRadioStationNumber (5);
@@ -41,6 +78,19 @@ public class RadioTest {
 
         int actual = rad.getCurrentRadioStationNumber ();
         int expected = 4;
+
+        assertEquals (expected, actual);
+    }
+
+    @Test
+    void less() {
+        Radio rad = new Radio ();
+        rad.setCurrentRadioStationNumber (-1);
+
+        rad.prevRadioStationNumber ();
+
+        int actual = rad.getCurrentRadioStationNumber ();
+        int expected = 9;
 
         assertEquals (expected, actual);
     }
